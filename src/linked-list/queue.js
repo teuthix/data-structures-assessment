18d1dd78f30255d0c73114f10aacb432f43ab592
wey <pukaimakaola@gmail.com>
2023-12-28 21:14:34 -0500

    initial commit and passes some queue tests

---
+++ src/linked-list/queue.js
@@ -0,0 +1,42 @@
+const LinkedList = require("./linkedList");
+
+/**
+ * Implement a Queue using nothing more than a LinkedList.
+ */
+
+class Queue {
+  constructor() {
+    this.linkedList = new LinkedList();
+  }
+
+  enqueue(value) {
+    if (this.linkedList.length === 0) {
+      this.linkedList.insertAtHead(value);
+    } else {
+      this.linkedList.insert(value);
+    }
+  }
+
+  dequeue() {
+    //     console.log(this.linkedList.head.value);
+    return this.linkedList.remove(
+      (node) => node.value === this.linkedList.head.value
+    );
+  }
+
+  peek() {
+    if (this.linkedList.length > 0) {
+      return this.linkedList.head.value;
+    }
+  }
+
+  isEmpty() {
+    if (this.linkedList.length === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+module.exports = Queue;
