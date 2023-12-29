98406266c648099dbaaf5ffaac0b55e59aece04b
wey <pukaimakaola@gmail.com>
2023-12-29 01:17:48 -0500

    passes removeDuplicates

--- src/linked-list/removeDuplicates.js
+++ src/linked-list/removeDuplicates.js
@@ -12,6 +12,24 @@
 
 function removeDuplicates(sortedLinkedList) {
   // TODO: implement an algorithm to remove duplicate values from a sorted linked list.
+  // move it all to a map
+  // then move it back into a linked list
+  const noDupes = new Map();
+  if (sortedLinkedList.length === 0) {
+    return [];
+  } else if (sortedLinkedList.length === 1) {
+    return sortedLinkedList;
+  }
+
+  let current = sortedLinkedList.head;
+
+  while (current && current.next) {
+    if (current.value === current.next.value) {
+      current.next = current.next.next;
+    } else {
+      current = current.next;
+    }
+  }
 
   return sortedLinkedList;
 }
